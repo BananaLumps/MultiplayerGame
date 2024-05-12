cf12213a92499ab04ac5d976f48d0531850b21f6
Michael Donnelly <michaeld1992@hotmail.com>
2024-05-12 20:52:48 +1200

    Downgrade Fishnet to 4.2.2

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -6,12 +6,10 @@ namespace Base.Player
     public class Player : NetworkBehaviour
     {
         public readonly SyncVar<string> UserID = new();
-        // public readonly SyncList<string> UserNames = new();
-
 
         private void Awake()
         {
-            UserID.val
+
         }
         // Start is called before the first frame update
         void Start()
