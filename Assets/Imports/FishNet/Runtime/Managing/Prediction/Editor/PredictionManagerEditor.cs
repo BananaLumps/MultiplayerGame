c38a0d046c0a46f700178d3adad09a460856b434
Michael Donnelly <michaeld1992@hotmail.com>
2024-05-12 20:30:16 +1200

    Updated Fishnet
    Updated Fishnet to 4.3.0. Removed Master Server Toolkit due to errors with Fishnet

--- Assets/Imports/FishNet/Runtime/Managing/Prediction/Editor/PredictionManagerEditor.cs
+++ Assets/Imports/FishNet/Runtime/Managing/Prediction/Editor/PredictionManagerEditor.cs
@@ -1,4 +1,74 @@
-﻿#if UNITY_EDITOR
+﻿#if !PREDICTION_1
+#if UNITY_EDITOR
+using UnityEditor;
+using UnityEngine;
+
+namespace FishNet.Managing.Predicting.Editing
+{
+
+
+    [CustomEditor(typeof(PredictionManager), true)]
+    [CanEditMultipleObjects]
+    public class PredictionManagerEditor : Editor
+    {
+       // private SerializedProperty _queuedInputs;
+        private SerializedProperty _dropExcessiveReplicates;
+        private SerializedProperty _maximumServerReplicates;
+        private SerializedProperty _maximumConsumeCount;
+        private SerializedProperty _clientInterpolation;
+        private SerializedProperty _serverInterpolation;
+
+        protected virtual void OnEnable()
+        {
+            _dropExcessiveReplicates = serializedObject.FindProperty(nameof(_dropExcessiveReplicates));
+            _maximumServerReplicates = serializedObject.FindProperty(nameof(_maximumServerReplicates));
+            _maximumConsumeCount = serializedObject.FindProperty(nameof(_maximumConsumeCount));
+            _clientInterpolation = serializedObject.FindProperty(nameof(_clientInterpolation));
+            _serverInterpolation = serializedObject.FindProperty(nameof(_serverInterpolation));
+        }
+
+        public override void OnInspectorGUI()
+        {
+            serializedObject.Update();
+
+            GUI.enabled = false;
+            EditorGUILayout.ObjectField("Script:", MonoScript.FromMonoBehaviour((PredictionManager)target), typeof(PredictionManager), false);
+            GUI.enabled = true;
+
+
+            EditorGUILayout.LabelField("Client", EditorStyles.boldLabel);
+            EditorGUI.indentLevel++;
+            EditorGUILayout.PropertyField(_clientInterpolation);
+            EditorGUI.indentLevel--;
+            EditorGUILayout.Space();
+
+            EditorGUILayout.LabelField("Server", EditorStyles.boldLabel);
+            EditorGUI.indentLevel++;
+            EditorGUILayout.PropertyField(_serverInterpolation);
+            EditorGUILayout.PropertyField(_dropExcessiveReplicates);
+            EditorGUI.indentLevel++;
+            if (_dropExcessiveReplicates.boolValue == true)
+            {
+                EditorGUI.indentLevel++;
+                EditorGUILayout.PropertyField(_maximumServerReplicates);
+                EditorGUI.indentLevel--;
+            }
+            EditorGUI.indentLevel--;
+
+
+            serializedObject.ApplyModifiedProperties();
+        }
+
+    }
+}
+#endif
+
+
+#else
+
+
+
+#if UNITY_EDITOR
 using UnityEditor;
 using UnityEngine;
 
@@ -13,21 +83,14 @@ namespace FishNet.Managing.Predicting.Editing
         private SerializedProperty _queuedInputs;
         private SerializedProperty _dropExcessiveReplicates;
         private SerializedProperty _maximumServerReplicates;
-        private SerializedProperty _maximumConsumeCount;
         private SerializedProperty _redundancyCount;
-        private SerializedProperty _allowPredictedSpawning;
-        private SerializedProperty _reservedObjectIds;
-
 
         protected virtual void OnEnable()
         {
             _queuedInputs = serializedObject.FindProperty(nameof(_queuedInputs));
             _dropExcessiveReplicates = serializedObject.FindProperty(nameof(_dropExcessiveReplicates));
             _maximumServerReplicates = serializedObject.FindProperty(nameof(_maximumServerReplicates));
-            _maximumConsumeCount = serializedObject.FindProperty(nameof(_maximumConsumeCount));
             _redundancyCount = serializedObject.FindProperty(nameof(_redundancyCount));
-            _allowPredictedSpawning = serializedObject.FindProperty(nameof(_allowPredictedSpawning));
-            _reservedObjectIds = serializedObject.FindProperty(nameof(_reservedObjectIds));
         }
 
         public override void OnInspectorGUI()
@@ -38,40 +101,29 @@ namespace FishNet.Managing.Predicting.Editing
             EditorGUILayout.ObjectField("Script:", MonoScript.FromMonoBehaviour((PredictionManager)target), typeof(PredictionManager), false);
             GUI.enabled = true;
 
+
+            EditorGUILayout.LabelField("Settings", EditorStyles.boldLabel);
+            EditorGUI.indentLevel++;
+            EditorGUILayout.PropertyField(_redundancyCount);
+            EditorGUILayout.PropertyField(_queuedInputs);
+            EditorGUI.indentLevel--;
+            EditorGUILayout.Space();
+
             EditorGUILayout.LabelField("Server", EditorStyles.boldLabel);
             EditorGUI.indentLevel++;
-            EditorGUILayout.PropertyField(_queuedInputs);
             EditorGUILayout.PropertyField(_dropExcessiveReplicates);
             EditorGUI.indentLevel++;
             if (_dropExcessiveReplicates.boolValue == true)
-            {
                 EditorGUILayout.PropertyField(_maximumServerReplicates);
-            }
-            else
-            {
-                EditorGUILayout.PropertyField(_maximumConsumeCount);
-            }
-            EditorGUI.indentLevel--;
             EditorGUI.indentLevel--;
 
-            EditorGUILayout.LabelField("Client", EditorStyles.boldLabel);
-            EditorGUI.indentLevel++;
-            EditorGUILayout.PropertyField(_redundancyCount);
-            EditorGUI.indentLevel--;
-
-            EditorGUILayout.PropertyField(_allowPredictedSpawning);
-            if (_allowPredictedSpawning.boolValue == true)
-            {
-                EditorGUI.indentLevel++;
-                EditorGUILayout.PropertyField(_reservedObjectIds);
-                EditorGUI.indentLevel--;
-            }
-
-            EditorGUILayout.Space();
 
             serializedObject.ApplyModifiedProperties();
         }
 
     }
 }
+#endif
+
+
 #endif
--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -1,16 +1,17 @@
 using FishNet.Object;
+using FishNet.Object.Synchronizing;
 
 namespace Base.Player
 {
     public class Player : NetworkBehaviour
     {
-        //public readonly SyncType<string> UserID;
+        public readonly SyncVar<string> UserID = new();
         // public readonly SyncList<string> UserNames = new();
 
 
         private void Awake()
         {
-
+            UserID.val
         }
         // Start is called before the first frame update
         void Start()
