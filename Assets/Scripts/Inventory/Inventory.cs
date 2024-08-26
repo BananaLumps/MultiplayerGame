ac1a17d44ba161df2b1a1aa672ec0272f26a974d
Michael Donnelly <michaeld1992@hotmail.com>
2024-08-26 17:59:52 +1200

    Commented all functions

--- Assets/Scripts/Inventory/Inventory.cs
+++ Assets/Scripts/Inventory/Inventory.cs
@@ -12,37 +12,78 @@ namespace Base.Inventory
 {
     public class Inventory : NetworkBehaviour
     {
-        [SerializeField]
-        GameObject UIObject;
-        public string UserID;
-        int size = 16;
-        Dictionary<string, InventoryItem> _items = new();
-        Dictionary<int, int> itemLocation = new();
-        public Dictionary<string, InventoryItem> Items => _items;
-        public Dictionary<int, int> ItemLocation => itemLocation;
+        /// <summary>
+        /// The ID of the owner player or set to "Server" if it is not a player inventory
+        /// </summary>
+        public string UserID
+        {
+            get
+            {
+                return UserID;
+            }
+            private set
+            {
+                if (value == string.Empty) UserID = "Server";
+                else UserID = value;
+            }
+        }
+        /// <summary>
+        /// How many total slots the inventory has.
+        /// </summary>
+        public int size { get; private set; } = 16;
+        /// <summary>
+        /// Dictionary of all inventory items.
+        /// <Key>Slot Number</Key>
+        /// <Value>InventoryItem</Value>
+        /// </summary>
+        public Dictionary<int, InventoryItem> Items
+        {
+            get; private set;
+        }
+        /// <summary>
+        /// Invoked when the inventory changes.
+        /// </summary>
         public static EventHandler<InventoryEventArgs> InventoryEvent;
-
-        //TODO: Fix creation of inventory and empty slots.
-        public void AddItem(string itemID, int amount)
+        /// <summary>
+        /// Add an item to inventory by ID. Amount defaults to one.
+        /// </summary>
+        /// <param name="itemID"></param>
+        /// <param name="amount"></param>
+        public void AddItem(string itemID, int amount = 1)
         {
             AddItem(new InventoryItem(itemID, amount));
         }
-
-        public void AddEquipment(string itemID, int amount, float durability)
+        /// <summary>
+        /// Add an equipment item to inventory by ID. Amount defaults to 1, Durability defaults to 0.
+        /// </summary>
+        /// <param name="itemID"></param>
+        /// <param name="amount"></param>
+        /// <param name="durability"></param>
+        public void AddEquipment(string itemID, float durability = 0, int amount = 1)
         {
             AddItem(new InventoryEquipment(itemID, amount, durability));
         }
-
-        public void AddConsumable(string itemID, int amount, int doses)
+        /// <summary>
+        /// Add a consumable item to inventory by ID. Amount and doses default to 1.
+        /// </summary>
+        /// <param name="itemID"></param>
+        /// <param name="doses"></param>
+        /// <param name="amount"></param>
+        /// <param name=""></param>
+        public void AddConsumable(string itemID, int doses = 1, int amount = 1)
         {
             AddItem(new InventoryConsumable(itemID, amount, doses));
         }
-        void AddItem(InventoryItem item)
+        /// <summary>
+        /// Add existing InventoryItem to inventory.
+        /// </summary>
+        /// <param name="item"></param>
+        public void AddItem(InventoryItem item)
         {
             int fullAmount = item.Count;
             if (item.Count <= 0) return;
             int stackSize = item.ItemObject.MaxStack;
-            foreach (var i in _items)
+            foreach (var i in Items)
             {
                 if (item.Count <= 0) break;
                 if (i.Value.ItemObject.ID == item.ItemObject.ID && i.Value.Count < stackSize)
@@ -54,73 +95,77 @@ namespace Base.Inventory
                 }
             }
             if (item.Count <= 0) return;
-            foreach (var i in _items)
+            foreach (var i in Items)
             {
                 if (i.Value.ItemObject.ID == "Empty")
                 {
                     if (item.Count <= stackSize)
                     {
-                        _items[i.Key] = item; break;
+                        Items[i.Key] = item; break;
                     }
                     else
                     {
-                        _items[i.Key] = new InventoryItem(item.ItemObject.ID, stackSize);
+                        Items[i.Key] = new InventoryItem(item.ItemObject.ID, stackSize);
                         item.Count -= stackSize;
                         continue;
                     }
                 }
 
             }
-            if (_items.Count == 0) _items.Add(item.ItemObject.ID, item);
             InventoryEvent?.Invoke(this, new InventoryEventArgs(UserID, item.ItemObject.ID, item.Count, InventoryEvents.Added));
         }
-        void RemoveItem(InventoryItem item)
+        /// <summary>
+        /// Remove existing InventoryItem from the inventory.
+        /// </summary>
+        /// <param name="item"></param>
+        public bool RemoveItem(InventoryItem item)
         {
-            if (item.Count <= 0) return;
-            foreach (var i in _items)
+            if (item.Count <= 0) return false;
+            if (AvailableItemAmount(item.ItemObject.ID) < item.Count) return false;
+            foreach (var i in Items)
             {
-                if (i.Value.ItemObject.ID != item.ItemObject.ID) continue;
-                if (item.Count < i.Value.Count)
+                if (i.Value.ItemObject.ID == item.ItemObject.ID || i.Value.ItemObject.ID == "Empty")
                 {
-                    i.Value.Count -= item.Count;
-                    break;
+                    if (item.Count < i.Value.Count)
+                    {
+                        i.Value.Count -= item.Count;
+                        break;
+                    }
+                    if (item.Count == i.Value.Count)
+                    {
+                        Items[i.Key] = new InventoryItem("Empty", 1);
+                        break;
+                    }
+                    if (item.Count > item.ItemObject.MaxStack)
+                    {
+                        Items[i.Key] = new InventoryItem("Empty", 1);
+                        item.Count -= item.ItemObject.MaxStack;
+                    }
                 }
-                if (item.Count == i.Value.Count)
-                {
-                    _items[i.Key] = new InventoryItem("Empty", 1);
-                    break;
-                }
-                if (item.Count > item.ItemObject.MaxStack)
-                {
-                    _items[i.Key] = new InventoryItem("Empty", 1);
-                    item.Count -= item.ItemObject.MaxStack;
-                }
-
             }
+            return true;
         }
-        private void Start()
+        /// <summary>
+        /// Call this after creation to set the UserID of the inventory. Pass string.Empty if it is not a player inventory and it will be set to "Server".
+        /// </summary>
+        /// <param name="userID"></param>
+        public void Init(string userID)
         {
-            UserID = Core.Instance.LocalPlayer.UserID.Value;
-            UserInput.InventoryKeyPressed += UserInput_InventoryKeyPressed;
-            UIObject = GameObject.Find("InventoryPanel");
+            UserID = userID;
             for (int i = 0; i < size; i++)
             {
-                Items.Add("Empty" + i.ToString(), new InventoryItem("Empty", 1));
+                Items.Add(i, new InventoryItem("Empty", 1));
             }
         }
-        public override void OnStartClient()
-        {
-
-        }
-        private void UserInput_InventoryKeyPressed()
-        {
-            UIObject.SetActive(!UIObject.activeInHierarchy);
-        }
-
+        /// <summary>
+        /// Returns the number of the specified item by ID, currently inside the inventory.
+        /// </summary>
+        /// <param name="itemID"></param>
+        /// <returns></returns>
         public int AvailableItemAmount(string itemID)
         {
             int amount = 0;
-            foreach (var i in _items)
+            foreach (var i in Items)
             {
                 if (i.Value.ItemObject.ID == itemID)
                 {
@@ -129,22 +174,30 @@ namespace Base.Inventory
             }
             return amount;
         }
-
+        /// <summary>
+        /// Returns the number of empty slots.
+        /// </summary>
+        /// <param name="nc"></param>
+        /// <returns></returns>
         public int EmptySlotAmount(NetworkConnection nc = null)
         {
             if (base.Owner != nc) return -1;
             int amount = 0;
-            foreach (var i in _items)
+            foreach (var i in Items)
             {
                 if (i.Value.ItemObject.ID == "Empty") amount++;
             }
             return amount;
         }
-
+        /// <summary>
+        /// Returns how much of the specified item can fit inside the inventory. Takes into account item stack and empty slots.
+        /// </summary>
+        /// <param name="itemID"></param>
+        /// <returns></returns>
         public int AvailableItemSpace(string itemID)
         {
             int space = 0;
-            foreach (var i in _items)
+            foreach (var i in Items)
             {
                 if (i.Value.ItemObject.ID == itemID)
                 {
@@ -155,11 +208,12 @@ namespace Base.Inventory
         }
         private void OnDestroy()
         {
-            // _items.Clear();
-            itemLocation.Clear();
-            UserInput.InventoryKeyPressed -= UserInput_InventoryKeyPressed;
+            Items.Clear();
         }
     }
+    /// <summary>
+    /// Returns the player, the item and the amount processed. EventType specifies if it was added or removed.
+    /// </summary>
     public class InventoryEventArgs : EventArgs
     {
         public string UserID;
@@ -175,6 +229,9 @@ namespace Base.Inventory
             EventType = eventType;
         }
     }
+    /// <summary>
+    /// Tracks if item in the event was added or removed.
+    /// </summary>
     public enum InventoryEvents : int
     {
         None = 0,
